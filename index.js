c667ef33ec864ea5343f1957b807916e11ccdeb0
Mario Orellana <mario@journeyhero.net>
2023-04-11 23:33:37 -0600

    Add emoji to differentiate request and response in console

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ import axios from 'axios';
 
 const app = express();
 app.use(cors());
-app.use(json());
+app.use(express.json());
 
 const coala = axios.create({
     headers: {
@@ -20,14 +20,14 @@ app.route('/coala/*')
   
         const endpoint = req.originalUrl.replace('/coala/', '');
 
-        console.log(`(${reqId}) GET Request:`, endpoint);
+        console.log(`➡ (${reqId}) GET Request:`, endpoint);
         coala.get(endpoint)
             .then(response => {
-                console.log(`(${reqId}) GET Response:`, response.data);
+                console.log(`⬅ (${reqId}) GET Response:`, response.data);
                 res.json(response.data);
             })
             .catch(error => {
-                console.log('Error on GET request:', endpoint);
+                console.log(`⬅ (${reqId}) Error on GET request:`, endpoint);
                 res.sendStatus(error.response.status);
             });
     })
@@ -37,14 +37,14 @@ app.route('/coala/*')
         const endpoint = req.originalUrl.replace('/coala/', '');
         const payload = req.body;
 
-        console.log(`(${reqId}) POST Request:`, endpoint, payload);
+        console.log(`➡ (${reqId}) POST Request:`, endpoint, payload);
         coala.post(endpoint, payload)
             .then(response => {
-                console.log(`(${reqId}) POST Response:`, response.data);
+                console.log(`⬅ (${reqId}) POST Response:`, response.data);
                 res.send(response.data);
             })
             .catch(error => {
-                console.log('Error on POST request:', endpoint, payload);
+                console.log(`⬅ (${reqId}) Error on POST request:`, endpoint, payload);
                 res.sendStatus(error.response.status);
             });
     });
